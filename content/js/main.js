0f9200e2288f8bcd35e536343b9eacfe8e2f77be
Julia <46110437+juliawukeke@users.noreply.github.com>
2019-10-19 16:14:01 +0800

    新增ＪＳ

--- content/js/main.js
+++ content/js/main.js
@@ -1,5 +1,19 @@
 $(function () {
+
     AOS.init({
         once: true
     });
+
+    //Header
+    $('[data-trigger]').on('click', function () {
+        $(this).parents('.l-container').toggleClass('is-active');
+    });
+
+    var query = Modernizr.mq('(max-width: 767px)');
+    if (query) {
+        $('.l-menu li').on('click', 'a', function () {
+            $(this).parents('.l-container').removeClass('is-active');
+        })
+    }
+
 });
